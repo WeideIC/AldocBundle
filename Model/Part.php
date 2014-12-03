2dbf7faaac55e558b2354a07c9459232a732a67e
Bart Guliker <bart@weide-ic.nl>
2014-12-03 09:52:32 +0000

    Initial commit

---
+++ Model/Part.php
@@ -0,0 +1,15 @@
+<?php
+namespace Weide\AldocBundle\Model;
+
+class Part
+{
+    protected $article;
+    protected $type;
+    protected $part;
+    protected $sup;
+    protected $remref;
+    protected $prijs;
+    protected $parent;
+    protected $partcode;
+    protected $supcode;
+}
---
+++ Model/Type.php
@@ -0,0 +1,19 @@
+<?php
+namespace Weide\AldocBundle\Model;
+
+class Type
+{
+    protected $carcode;
+    protected $make;
+    protected $modelcode;
+    protected $model;
+    protected $modelrem;
+    protected $yearStart;
+    protected $yearEnd;
+    protected $typeCode;
+    protected $type;
+    protected $motorType;
+    protected $fuel;
+    protected $power;
+    protected $power2;
+}
