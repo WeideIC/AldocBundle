796205b9543a046e54f92597144ad08b67487f17
Bart Guliker <bart@weide-ic.nl>
2014-12-03 13:19:47 +0100

    Fixed const reference. Rebuilt interface as abstract class to pre-implement some methods

--- CacheResolver/PartsCacheResolver.php
+++ CacheResolver/PartsCacheResolver.php
@@ -1,52 +1,9 @@
 <?php
 namespace Weide\AldocBundle\CacheResolver;
 
-class PartsCacheResolver implements CacheResolverInterface
+class PartsCacheResolver extends AbstractCacheResolver
 {
     const TTL = 86400;
-
-    public function isCached($key)
-    {
-        $filename = $this->getCacheFilename($key);
-
-        if(file_exists($filename))
-        {
-            if(filemtime($filename) < (time() - self::TTL))
-            {
-                $this->invalidate($key);
-                return false;
-            }
-
-            return true;
-        }
-
-        return false;
-    }
-
-    public function getCached($key)
-    {
-        if(!$this->isCached($key))
-        {
-            return false;
-        }
-
-        return file_get_contents($this->getCacheFilename($key));
-    }
-
-    public function setCache($key, $data)
-    {
-        return file_put_contents($this->getCacheFilename($key), $data);
-    }
-
-    public function invalidate($key)
-    {
-        if(!$this->isCached($key))
-        {
-            return false;
-        }
-
-        @unlink($this->getCacheFilename($key));
-    }
 
     public function generateKey(array $data)
     {
